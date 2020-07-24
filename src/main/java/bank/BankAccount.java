42855e0c2b09b5ba5e01bfd44e6ee5c5ba00c396
gheidimahdi <gheidimahdi@gmail.com>
2020-07-24 11:09:06 +0430

    BankAccount done

--- src/main/java/bank/BankAccount.java
+++ src/main/java/bank/BankAccount.java
@@ -1,4 +1,49 @@
 package bank;
 
+import java.time.LocalDate;
+
 public class BankAccount {
+    String firstName;
+    String lastName;
+    String accountNumber;
+    String password;
+    LocalDate expirationDate;
+    int deposit;
+
+    public BankAccount(String firstName, String lastName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = generateAccountNumber();
+        this.password = password;
+        this.expirationDate = LocalDate.now().plusYears(3);
+        this.deposit = 0;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public synchronized long getMoney() {
+        return this.deposit;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public synchronized void setMoney(int money) {
+        this.deposit = money;
+    }
+
+    public boolean isValid(String accountNumber, String password){
+        return accountNumber.equals(this.accountNumber) && password.equals(this.password);
+    }
+
+    private String generateAccountNumber(){
+        String accountNum = new String();
+        for(int i = 0; i < 16; i++)
+            accountNum.concat(Integer.toString(((int)Math.random())%10));
+        return accountNum;
+    }
+
 }
